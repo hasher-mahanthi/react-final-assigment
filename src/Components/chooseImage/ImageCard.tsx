87b204422c088b4858edbda92ab3592b61fd0273
hasher-mahanthi <mahanthi.kumar@hashedin.com>
2020-12-04 18:20:09 +0530

    added grid to the cards

--- src/Components/chooseImage/ImageCard.tsx
+++ src/Components/chooseImage/ImageCard.tsx
@@ -7,46 +7,48 @@ function ImageCard(props:any) {
     }
             
     
-        return(
-            <div className="image-card-list">
+//         return(
+//             <div className="image-card-list">
             
-            <div className="image-card">
-                <div className="image-block"></div>
-                <h2 className="image-title">{props.name}</h2>
-                <p className="image-description">{props.description}</p>
-                <div className="radio-btn">
-                    <input type="radio"/>64 bit (x86)<br></br>
-                    <input type="radio"/>64 bit (ARM)
-                </div>
-                <div className="select-button-position">
-                 <button className="select-button" onClick={setCard}>Select</button>
-                 </div>   
-            </div>
-                
-        </div>
-)
-
-            
-//  <div className="image-card-list">
-                
-//                 <div className=" image-card">
-//                     <div className="image-block"></div>
-//                    <h2 className="image-title" >{props.name}</h2>
-//                     <p className="image-description">arun is a gd bot and having 3 pairs</p>
-//                         <div className="image-buttons d-flex flex-column">
-//                               <div className="d-flex flex-column">
-//                                  <input className='image-radio-btn'type="radio"/>64 bit (x86)<br></br>
-//                                   <input className='image-radio-btn'type="radio"/>64 bit (ARM)
-//                                </div>
-//                         <div >
-//                         <button className="select-button" >Select</button>
-//                         </div>  
-//                         </div>
+//             <div className="image-card">
+//                 <div className="image-block"></div>
+//                 <h2 className="image-title">{props.name}</h2>
+//                 <p className="image-description">{props.description}</p>
+//                 <div className="radio-btn">
+//                     <input type="radio"/>64 bit (x86)<br></br>
+//                     <input type="radio"/>64 bit (ARM)
 //                 </div>
-
-//         </div> )
+//                 <div className="select-button-position">
+//                  <button className="select-button" onClick={setCard}>Select</button>
+//                  </div>   
+//             </div>
+                
+//         </div>
+// )
+return(
+    <div className="image-card-list">
+    
+    <div className="image-card row ">
+        <div className="image-block col-2"></div>
+        <div className='col-8'>
+        <h2 >{props.name}</h2>
+        <p >{props.description}</p>
+        </div>
+        <div className='col-2'>
+        <div >
+            <input type="radio"/>64 bit (x86)<br></br>
+            <input type="radio"/>64 bit (ARM)
+        </div>
+        <div >
+         <button  onClick={setCard}>Select</button>
+         </div> 
+         </div>  
+    </div>
         
-        // );
+</div>
+)
+            
+
         
     
 }
