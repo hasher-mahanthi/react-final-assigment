07a58e4c53bfe9496bc244ccb5ee3dcf85d23d6a
hasher-mahanthi <mahanthi.kumar@hashedin.com>
2020-12-04 07:28:53 +0530

    added the header and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+//for the style of grid refer the app which you have made
 function App() {
+
+  const[page,setPage]=useState(0);
+
+
+  const setPage1 = () => {
+    setPage(1);
+  }
+  const setPage2 = () => {
+    setPage(2);
+  }
+  const setPage3 = () => {
+    setPage(3);
+  }
+  const setPage4 = () => {
+    setPage(4);
+  }
+  const setPage5 = () => {
+    setPage(5);
+  }
+  
+  function getPageName(){
+    let pageName;
+    switch(page){
+    case 1:
+      pageName = "Choose Image";
+      break;
+      case 2:
+      pageName = "Choose Instance Type";
+      break;
+      case 3:
+      pageName = "Choose Storage and Network";
+      break;
+      case 4:
+      pageName = "Configure Security";
+      break;
+      case 5:
+        pageName = "Review and Launch";
+        break;
+    default:
+      pageName = "Choose Image";
+  }
+  return pageName;
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>HVC</h2>
       </header>
+      {/* <div className="container"> */}
+  <div className="row">
+    <div className="col-9">
+      <div className="content-header d-flex justify-content-between">
+      <div>
+       { getPageName()}
+        </div>
+        <div>
+        <select placeholder="Region">
+            <option value="us-east-1<">us-east-1</option>
+            <option value="us-east-2">us-east-2</option>
+            <option value="us-west-1">us-west-1</option>
+            <option value="india">india</option>
+          </select>
+          </div>
+      </div>
+      <div>
+      <nav className="navbar">
+            <button className={page===1?"navButton":''} onClick={setPage1}>1.Choose Image</button>
+            <button className={page===2?"navButton":''} onClick={setPage2}>2.Choose Instance Type</button>
+            <button className={page===3?"navButton":''} onClick={setPage3}>3.Choose Storage and Network</button>
+            <button className={page===4?"navButton":''} onClick={setPage4}>4.Configure Security</button>
+            <button className={page===5?"navButton":''} onClick={setPage5}>5.Review and Launch</button>
+          </nav>  
+      </div>
     </div>
+    <div className="col-3"> 
+      2 of 3 (wider)
+    </div>
+  </div>
+  </div>
+    // </div>
   );
 }
 
